9bb71977bfef562c23fb91cc45f4e4036f801614
Timothy John Gandionco <tim.gandionco@gmail.com>
2024-11-21 04:21:30 +0800

    debugging enabled

--- wp-config.php
+++ wp-config.php
@@ -79,7 +79,10 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'SCRIPT_DEBUG', true );
+define( 'SAVEQUERIES', true );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
