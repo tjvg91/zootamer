0548c3606d5ac8b5267549c37a06ba13004258cb
Timothy John Gandionco <tim.gandionco@gmail.com>
2024-12-25 13:48:10 +0800

    rearrange quantity for non-video service

--- wp-content/themes/twentytwentyfour/js/my-actions.js
+++ wp-content/themes/twentytwentyfour/js/my-actions.js
@@ -5,21 +5,24 @@ jQuery(function() {
 		}, 900,'swing');
 	}) 
 
-	jQuery('[name="merchant-buy-now"], .single_add_to_cart_button').wrap('<div style="display: flex; justify-content: flex-end;"></div>');
+	//jQuery('[name="merchant-buy-now"], .screen-reader-text, .input-text.qty.text, .single_add_to_cart_button, [name="is-descendent-of-single-product-block"]')
+	jQuery('[name="merchant-buy-now"], .wp-block-woocommerce-add-to-cart-form form.cart>.quantity, .single_add_to_cart_button')
+		.wrapAll('<div class="quantity-cart-container" style="display: flex; justify-content: flex-end;"></div>');
 	scrollToMainSection();
 
-	let quantityContainer = jQuery(".woocommerce div.product .wc-block-add-to-cart-form form.cart .quantity");
+	let quantityContainer = jQuery(".wp-block-woocommerce-add-to-cart-form form.cart .quantity");
 
 	if(/^\?product=(day-to-dusk|item-removal|virtual-staging)$/.test(window.document.location.search)) {
-		quantityContainer.detach().appendTo(".number-of-files-to-deliver");
+		quantityContainer.css("cssText", "");
 	} else if(/^\?product=satellite-marketing-video$/.test(window.document.location.search)){
-		quantityContainer.hide();
+		quantityContainer.css("cssText", "display: none !important");
 	}
 });
 
 function scrollToMainSection() {
 	let visited = window.localStorage.getItem("visited");
-	if(visited && !!jQuery('#services-container')) {
+	const serviceContainer = jQuery('#services-container')
+	if(visited && !!serviceContainer.top) {
 		jQuery('html, body').animate({
 			scrollTop: jQuery('#services-container').offset().top - 50
 		}, 900,'swing');
